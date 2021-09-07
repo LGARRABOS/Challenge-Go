f343673ecbc2c74ee7253f7c1b08b95b4103e1cb
LGARRABOS <lus.garrabos@ynov.com>
2021-09-07 17:09:26 +0200

    exo2

---
+++ printreversealphabet/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	alphabet := "abcdefghijklmnopqrstuvwxyz"
+	for i := 25; i > len(alphabet); i++ {
+		z01.PrintRune(rune(alphabet[i]))
+	}
+
+	z01.PrintRune('\n')
+}
