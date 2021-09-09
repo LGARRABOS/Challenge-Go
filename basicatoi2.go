c2b8dd2fc43550f2abff02150af623bcefd4e57f
LGARRABOS <lus.garrabos@ynov.com>
2021-09-09 12:03:46 +0200

    exo

--- basicatoi2.go
+++ basicatoi2.go
@@ -3,13 +3,17 @@ package piscine
 func BasicAtoi2(s string) int {
 	o := 0
 	c := 0
-	nb_word := []rune(s)
-	for _, word := range nb_word {
-		for i := '0'; i < word; i++ {
-			c++
+	for _, word := range s {
+		if word >= 48 && word <= 57 {
+			for i := '0'; i < word; i++ {
+				c++
+			}
+			o = o*10 + c
+			c = 0
+		} else {
+			o = 0
+			break
 		}
-		o = o*10 + c
-		c = 0
 	}
 	return o
 }
