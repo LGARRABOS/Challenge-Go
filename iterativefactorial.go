4a87742eabae35725369b50199cf314766ca97d0
LGARRABOS <lus.garrabos@ynov.com>
2021-09-13 10:13:44 +0200

    exo

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -2,8 +2,14 @@ package piscine
 
 func IterativeFactorial(nb int) int {
 	result := 1
-	for i := 0; i < nb+1; i++ {
-		result = result * i
+	if nb >= 0 && nb < 100 {
+		for i := 1; i <= nb; i++ {
+			if result > 0 {
+				result = result * i
+			}
+		}
+	} else {
+		result = 0
 	}
 	return result
 }
