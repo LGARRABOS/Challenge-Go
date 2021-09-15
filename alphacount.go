08975d419ad82aaf817ea043f39fbac35584bde3
LGARRABOS <lus.garrabos@ynov.com>
2021-09-15 14:32:44 +0200

    exo

--- alphacount.go
+++ alphacount.go
@@ -3,8 +3,8 @@ package piscine
 func AlphaCount(s string) int {
 	i := []rune(s)
 	m := 0
-	for a := 0; a <= len(s); a++ {
-		if i[a] >= 'a' && i[a] <= 'z' && i[a] >= 'A' && i[a] <= 'Z' {
+	for a := 0; a < len(s); a++ {
+		if i[a] >= 'a' && i[a] <= 'z' || i[a] >= 'A' && i[a] <= 'Z' {
 			m++
 		}
 	}
