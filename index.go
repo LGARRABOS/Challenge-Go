b6563e77fbf63b03fd70f8534d43841cba491b76
LGARRABOS <lus.garrabos@ynov.com>
2021-09-15 17:05:56 +0200

    exo

--- index.go
+++ index.go
@@ -1,24 +1,26 @@
 package piscine
 
 func Index(s string, toFind string) int {
-	w := 0
+	count := 0
+	m := 0
 	z := []rune(toFind)
 	l := []rune(s)
-	for q, a := range s {
-		if w == 0 {
-			if a == z[0] {
-				w = q
+	for q, lll := range s {
+		if count == 0 {
+			if lll == z[0] {
+				count = q
+				m++
 				break
-			} else if q == len(s)-1 {
+			} else if q == len(s)-1 && m == 0 {
 				return -1
 			}
 		}
 	}
 	for i := 0; i < len(z); i++ {
-		if l[w+i] == z[i] {
+		if l[count+i] == z[i] {
 		} else {
 			return -1
 		}
 	}
-	return w
+	return count
 }
