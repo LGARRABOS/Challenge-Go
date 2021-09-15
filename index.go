63fc72659f1adf91fa4af0939551b45bc2ce7f8d
LGARRABOS <lus.garrabos@ynov.com>
2021-09-15 16:57:20 +0200

    exo

---
+++ index.go
@@ -0,0 +1,22 @@
+package piscine
+
+func Index(s string, toFind string) int {
+	w := 0
+	z := []rune(toFind)
+	l := []rune(s)
+	for q, a := range s {
+		if w == 0 {
+			if a == z[0] {
+				w = q
+				break
+			}
+		}
+	}
+	for i := 0; i < len(z); i++ {
+		if l[w+i] == z[i] {
+		} else {
+			return -1
+		}
+	}
+	return w
+}
