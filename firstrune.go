9aaf917f8b0fc8196983e782bb872ce12aad0d39
LGARRABOS <lus.garrabos@ynov.com>
2021-09-15 10:04:29 +0200

    exo

---
+++ firstrune.go
@@ -0,0 +1,7 @@
+package piscine
+
+func FirstRune(s string) rune {
+	i := []rune(s)
+	return (i[0])
+
+}
--- test/main.go
+++ test/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
-	"fmt"
+	"github.com/01-edu/z01"
+
 	"piscine"
 )
 
 func main() {
-	fmt.Println(piscine.Sqrt(4))
-	fmt.Println(piscine.Sqrt(3))
+	z01.PrintRune(piscine.FirstRune("Hello!"))
+	z01.PrintRune(piscine.FirstRune("Salut!"))
+	z01.PrintRune(piscine.FirstRune("Ola!"))
+	z01.PrintRune('\n')
 }
