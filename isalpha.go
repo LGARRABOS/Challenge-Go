e59ee36c5e617e3e2cc5a8505f7e586644d0665a
LGARRABOS <lus.garrabos@ynov.com>
2021-09-16 10:12:23 +0200

    exo

--- isalpha.go
+++ isalpha.go
@@ -5,8 +5,8 @@ func IsAlpha(s string) bool {
 	for a := 0; a < len(s); a++ {
 		if i[a] >= 'a' && i[a] <= 'z' || i[a] >= 'A' && i[a] <= 'Z' || i[a] >= '1' && i[a] <= '9' {
 		} else {
-			return true
+			return false
 		}
 	}
-	return false
+	return true
 }
