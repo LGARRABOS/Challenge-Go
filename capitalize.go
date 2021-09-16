6467f503809b3983c75dbe85d984daf125babe89
LGARRABOS <lus.garrabos@ynov.com>
2021-09-16 15:38:43 +0200

    exo

--- capitalize.go
+++ capitalize.go
@@ -6,7 +6,7 @@ func Capitalize(s string) string {
 		i[0] = i[0] - 32
 	}
 	for a := 1; a < len(s); a++ {
-		if i[a-1] >= 'a' && i[a]-1 <= 'z' || i[a-1] >= 'A' && i[a-1] <= 'Z' || i[a-1] >= '0' && i[a-1] <= '9' {
+		if i[a-1] >= 'a' && i[a-1] <= 'z' || i[a-1] >= 'A' && i[a-1] <= 'Z' || i[a-1] >= '0' && i[a-1] <= '9' {
 			if i[a] >= 'A' && i[a] <= 'Z' {
 				i[a] = i[a] + 32
 			}
