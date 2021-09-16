e8157dfc5b003feaf382e4930af44ed7e8bb3019
LGARRABOS <lus.garrabos@ynov.com>
2021-09-16 10:11:31 +0200

    exo

--- isalpha.go
+++ isalpha.go
@@ -3,7 +3,7 @@ package piscine
 func IsAlpha(s string) bool {
 	i := []rune(s)
 	for a := 0; a < len(s); a++ {
-		if i[a] >= 'a' && i[a] <= 'z' || i[a] >= 'A' && i[a] <= 'Z' || i[a] >= '1' && i[a] <= '9' || len(i) == 0 {
+		if i[a] >= 'a' && i[a] <= 'z' || i[a] >= 'A' && i[a] <= 'Z' || i[a] >= '1' && i[a] <= '9' {
 		} else {
 			return false
 		}
