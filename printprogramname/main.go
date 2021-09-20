8c6ff6a2cebd5bbf387b3841e996d966f9ba7984
LGARRABOS <lus.garrabos@ynov.com>
2021-09-20 10:30:49 +0200

    exo

--- printprogramname/main.go
+++ printprogramname/main.go
@@ -10,7 +10,7 @@ func main() {
 	a := os.Args[0]
 	z := []rune(a)
 	for i := 0; i < len(a); i++ {
-		if z[i] == ('.') || z[i] == ('/') {
+		if z[i] == '.' || z[i] == '/' {
 		} else {
 			z01.PrintRune(z[i])
 		}
