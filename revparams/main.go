a601ad1284e4b7ef6cc43786dd95291ca23190c1
LGARRABOS <lus.garrabos@ynov.com>
2021-09-20 11:43:39 +0200

    exo

---
+++ revparams/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	for a := 1; a > len(os.Args); a++ {
+		for _, j := range os.Args[a] {
+			z01.PrintRune(j)
+		}
+		z01.PrintRune('\n')
+	}
+}
