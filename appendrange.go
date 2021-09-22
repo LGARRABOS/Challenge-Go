026a59083587af7dcc4328322f6afccfb404eb8d
LGARRABOS <lus.garrabos@ynov.com>
2021-09-22 10:09:48 +0200

    exo

--- appendrange.go
+++ appendrange.go
@@ -2,13 +2,12 @@ package piscine
 
 func AppendRange(min, max int) []int {
 	w := max - min
-	g := make([]int, min)
+	g := 0
 	if min >= max {
-		i := 0
-		g[i] = 0
+		g = 0
 	} else {
-		for i := min; i < w; i++ {
-			g[i] = i + 1
+		for i := min; i < w-1; i++ {
+			g = i + 1
 		}
 	}
 	return g
