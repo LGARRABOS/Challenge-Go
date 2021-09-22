92140dc087fe09c92e118cba207ff94af72011c9
LGARRABOS <lus.garrabos@ynov.com>
2021-09-22 16:54:37 +0200

    exo

---
+++ splitwhitespaces.go
@@ -0,0 +1,20 @@
+package piscine
+
+func SplitWhiteSpaces(s string) []string {
+	var g []string
+	var m string
+	w := []rune(s)
+	for i := 0; i < len(s); i++ {
+		if w[i] == 32 || w[i] == '\n' || w[i] == 9 {
+			if w[i+1] == 32 || w[i+1] == '\n' || w[i+1] == 9 {
+			} else {
+				g = append(g, m)
+				m = ""
+			}
+		} else {
+			m = m + string(w[i])
+		}
+	}
+	g = append(g, m)
+	return g
+}
