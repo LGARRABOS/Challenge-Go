e04f6c74c10eab29c2a328a29c687257c4d225a1
LGARRABOS <garrabos.luc@ynov.com>
2021-09-29 10:47:06 +0200

    exo

--- issorted.go
+++ issorted.go
@@ -3,7 +3,7 @@ package piscine
 func IsSorted(f func(a, b int) int, a []int) bool {
 	j := 0
 	for i := 0; i < len(a)-1; i++ {
-		if i < i+1 {
+		if f(a[i], a[i]+1) >= 0 {
 			j = j + 1
 		} else {
 			break
