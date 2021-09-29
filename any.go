8a74e556a03e8a7b6584337750b2b6e4c074a86b
LGARRABOS <garrabos.luc@ynov.com>
2021-09-29 09:36:25 +0200

    exo

--- any.go
+++ any.go
@@ -4,9 +4,7 @@ func Any(f func(string) bool, a []string) bool {
 	for i := 0; i < len(a); i++ {
 		if f(a[i]) == true {
 			return true
-		} else {
-			return false
 		}
 	}
-	return true
+	return false
 }
