1f3db65d2687c1e66ace2da1e8e84d6e0cada8e9
LGARRABOS <garrabos.luc@ynov.com>
2021-09-29 09:14:49 +0200

    exo

--- any.go
+++ any.go
@@ -3,9 +3,9 @@ package piscine
 func Any(f func(string) bool, a []string) bool {
 	for i := 0; i < len(a); i++ {
 		if f(a[i]) == true {
-			return false
-		} else {
 			return true
+		} else {
+			return false
 		}
 	}
 	return true
